c0faa8c932efe3f3019d7ad05f2c906558d57b8f
Andes Court <andres@andrescourt.com>
2022-06-13 22:47:23 -0500

    app.use

---
+++ 8-app-use.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const logger = require('./logger')
+const app =express()
+
+
+// req => middleware => res
+// app.use orders matters
+// middleware functions first
+app.use(logger)
+
+app.get('/', (req, res) => {
+    res.send('Home')
+})
+
+app.get('/about', (req, res) => {
+    res.send('About')
+})
+
+app.get('/api/products', (req, res) => {
+    res.send('Products')
+})
+
+app.get('/api/items', (req, res) => {
+    res.send('Items')
+})
+
+app.listen(5000, () => {
+    console.log('Server listening in port 5000...');
+})
